2e0ecb556de15ad96b23cd8cfdfebbf73195292d
Mathias <aquemaati@gmail.com>
2026-07-29 17:49:53 +0200

    first commit

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ta mere',
-        ]);
+        $greet = '';
+        if ($name = $request->query->get('name')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        dump($request);
+        $method = $request->getMethod();
+        return new Response(<<<EOF
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Welcome!</title>
+                </head>
+                <body>
+                    $greet
+                    <p>Request method: $method</p>
+                    <img src="/images/under-construction.gif" />
+                </body>
+            </html>
+        EOF);
     }
 }
